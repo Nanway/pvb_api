0743bd7f27f205ae3ef838f52d93d24eff75d52f
Nanway Chen <nanway.chen@hotmail.com>
2019-01-27 19:39:46 +1100

    added cors

--- server.py
+++ server.py
@@ -1,7 +1,9 @@
 from flask import Flask
 from model import Dog_Classifier
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 print("Loading model")
 classifier = Dog_Classifier()
 print("model loaded")
